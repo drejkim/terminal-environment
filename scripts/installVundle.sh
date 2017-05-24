9dbddbd1cb937b1cfe68e9ee972fad7354ab6d8b
Esther Jun Kim <esther.jun.kim@gmail.com>
2017-05-23 17:49:16 -0700

    Add installVundle.sh

---
+++ scripts/installVundle.sh
@@ -0,0 +1,5 @@
+# Reference: https://github.com/VundleVim/Vundle.vim
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+echo "Vundle setup complete. Copy .vimrc to your home directory. Then, launch vim, and run :PluginInstall"
