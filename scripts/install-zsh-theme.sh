bc50dba7cf8d9da17ed11fb8e32e92382ea452db
Esther Jun Kim <esther.jun.kim@gmail.com>
2018-02-16 06:34:28 -0800

    Update install-zsh-theme.sh
    The hard-coded dracula paths have been replaced with the variable "DRACULA_DIR." A short explanatory comment describing the script has also been added.

--- scripts/install-zsh-theme.sh
+++ scripts/install-zsh-theme.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-if [ ! -d "~/config/dracula" ]; then
-    mkdir -p ~/config/dracula
-fi
+# Install the zsh dracula theme.
 
-cd ~/config/dracula
+DRACULA_DIR=~/config/dracula
+
+mkdir -p $DRACULA_DIR
+cd $DRACULA_DIR
 
 # Install Dracula theme
 git clone https://github.com/dracula/zsh.git
-ln -s ~/config/dracula/zsh/dracula.zsh-theme ~/.oh-my-zsh/themes/dracula.zsh-theme
+ln -s $DRACULA_DIR/zsh/dracula.zsh-theme ~/.oh-my-zsh/themes/dracula.zsh-theme
 
 echo "Setup complete. Set ZSH_THEME=\"dracula\" in .zshrc."
